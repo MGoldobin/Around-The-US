9aeac625813e12bbf5d3716a20dd7a6cd3c2fdc8
MGoldobin <MGoldobin@users.noreply.github.com>
2020-09-29 21:29:16 +0500

    create Section class

---
+++ src/scripts/Section.js
@@ -0,0 +1,17 @@
+class Section {
+  constructor ({items, renderer}, sectionSelector) {
+    this._items = items;
+    this._renderer = renderer;
+    this._section = document.querySelector(sectionSelector);
+  }
+
+  renderItems() {
+    this._items.forEach((item) => this._renderer(item));
+  }
+
+  addItem(element) {
+    this._section.append(element);
+  }
+}
+
+export default Section;
