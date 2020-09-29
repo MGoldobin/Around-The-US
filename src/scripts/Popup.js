e347200a4fcd4f8ce5ec955d1ce3a87b2f29eb87
MGoldobin <MGoldobin@users.noreply.github.com>
2020-09-29 21:30:02 +0500

    create Popup class

---
+++ src/scripts/Popup.js
@@ -0,0 +1,34 @@
+class Popup{
+	constructor(popupSelector) {
+		this._popupElement = document.querySelector(popupSelector);
+		this._handleEscClose = this._handleEscClose.bind(this);
+	}
+
+	open() {
+		this._popupElement.classList.add('popup_opened');
+		this._popupElement.classList.remove('popup_closed');
+		document.addEventListener('keydown', this._handleEscClose);
+	}
+
+	close() {
+		this._popupElement.classList.add('popup_closed');
+		this._popupElement.classList.remove('popup_opened');
+		document.removeEventListener('keydown', this._handleEscClose);
+	}
+
+	_handleEscClose(evt) {
+		if(evt.key === 'Escape'){
+      this.close();
+    }
+	}
+
+	setEventListeners() {
+		this._popupElement.addEventListener('click', (evt) => {
+			if(evt.target.classList.contains('popup__close-button') || (evt.target === this._popupElement)) {
+				this.close();
+			}
+		})
+	}
+}
+
+export default Popup;
