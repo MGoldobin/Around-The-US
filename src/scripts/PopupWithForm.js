8bd41ce6f3da125699f17d30eb0d66eaa28fb90a
MGoldobin <MGoldobin@users.noreply.github.com>
2020-09-29 21:29:53 +0500

    create PopupWithForm class

---
+++ src/scripts/PopupWithForm.js
@@ -0,0 +1,34 @@
+import Popup from './Popup.js';
+
+class PopupWithForm extends Popup {
+	constructor(popupSelector, submitForm) {
+		super(popupSelector);
+		this._submitForm = submitForm;
+		this._formElement = this._popupElement.querySelector(".popup__form");
+	}
+
+	_getInputValues() {
+		this._inputValues = {};
+		this._inputs = this._formElement.querySelectorAll(".popup__input");
+    this._inputs.forEach( (input) => {
+      this._inputValues[input.id] = input.value
+    });
+    return this._inputValues;
+	}
+
+	setEventListeners() {
+    this._formElement.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      this._submitForm(this._getInputValues);
+      this.close();
+    })
+    super.setEventListeners();
+  }
+  
+  close() {
+    super.close();
+    this._formElement.reset();
+  }
+}
+
+export default PopupWithForm;
